e679718cf15aa3805606373883717a5781b606e7
denden-tri <83654878+denden-tri@users.noreply.github.com>
2022-04-24 16:00:18 +0700

    menambahkan identitas pada kelas utama

--- app/src/main/java/com/denztri/monitory233/WelcomeActivity.java
+++ app/src/main/java/com/denztri/monitory233/WelcomeActivity.java
@@ -6,6 +6,26 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//  NIM                 : 10119233
+//  Nama                : Denden Triana
+//  Kelas               : IF-6
+//  Tanggal Pengerjaan  : 23-04-2022
+//  Deskripsi Pekerjaan : 1. Mengerjakan logic agar dapat berpindah dari WelcomeActivity ke
+//                           LoginCodeActivity
+//                        2. Membuat logic agar menutup semua activity jika mengklik tombol Done
+//                           pada DoneActivity
+//                        3. Mengerjakan logic agar dapat berpindah dari LoginCodeActivity ke
+//                           BiodataActivity
+//                        4. Membuat logic agar menampilkan dialog message jika TextInput Code nya
+//                           kosong/belum diisi
+//                        5. Mengerjakan logic agar dapat berpindah dari BiodataActivity ke
+//                           DoneActivity
+//                        6. Membuat logic agar DoneActivity dapat menerima data dari BiodataActivity
+//                           agar dapat ditampilkan pada DoneActivity
+//                        7. Membuat logic agar menampilkan dialog message jika TextInput Name pada
+//                           BiodataActivity kosong/belum diisi
+
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
